94b9411ff9a565b4d29fc88899982b793df8ccda
pekarsky.maxim@gmail.com <flufci@gmail.com>
2018-02-24 11:16:41 -0500

    prompt user to log in after creating github account

--- index.js
+++ index.js
@@ -450,7 +450,7 @@ MongoClient.connect(dbAddress, function(err, db){
                 if(response.status == "fail"){
                     res.render("index", { searchTerm: "", error: response.message});
                 } else {
-                    res.redirect("/");
+                    res.render("index", { searchTerm: "", message: "Go ahead and log in with Github!"});
                 }
             })   
         }
